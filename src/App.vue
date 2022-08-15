7ffd4f909a415cc15a845f34d4d49ab530473e89
Jimena <jimeblue69@gmail.com>
2022-08-15 18:50:07 +0200

    Finish template refs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+
+
 <template>
+<!-- EXPLANATION: TEMPLATE REFS are used to grab dom elements like we did querySelector in vanilla JS -->
 <h1>{{title}}</h1>
+<!-- EXAMPLE: -->
+<!-- EXPLANATION: give the input element a ref with a name -->
+<input type="text" ref="name">
+<!-- EXPLANATION: add on click event to the button and call the handleClick function -->
+<button @click="handleClick">Click me</button>
 </template>
 
 <script>
@@ -10,6 +18,14 @@ export default {
   return {
     title: "My fist Vue app 😇 "
   }
+ },
+ methods: {
+handleClick () {
+  // EXPLANATION: when btn is clicked console log the element with the ref name
+  console.log(this.$refs.name)
+    // EXPLANATION: when btn is clicked add the class active to the element with the ref name
+  this.$refs.name.classList.add("active")      
+}
  }
 }
 </script>
