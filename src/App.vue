656a8c2c3831a9b4545dcf8cb0e3cc6ac398549e
Jimena <jimeblue69@gmail.com>
2022-08-15 22:41:01 +0200

    Finish multiple components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-<h1>{{title}}</h1>
+  <!-- note: multiple components branch 23 -->
+
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-// object representing the root component
+// EXPLANATION: how to use the Modal component inside App component:
+// 1) Import the modal component
+// 2) Register that we want to use Modal inside App by creating a components property with an object as a value, and adding the component inside the object
+// 3) Use it inside the template
+
+import Modal from './components/Modal.vue';
 export default {
   name: 'App',
- data() {
-  return {
-    title: "My fist Vue app 😇 "
-  }
- }
-}
+  components: { Modal },
+  data() {
+    return {
+      title: 'My fist Vue app 😇 ',
+    };
+  },
+};
 </script>
 
 <style>
@@ -23,7 +32,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
-  border: 3px solid blue
+h1 {
+  border: 3px solid blue;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- note: this is the background of the modal -->
+  <div class="backdrop">
+    <!-- note: this is the modal -->
+    <div class="modal">
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
